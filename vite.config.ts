58970f84b1e8f073800c7b497a4580175a19ed9e
zjx-kimi <zjx-kimi@users.noreply.github.com>
2024-09-12 10:35:21 +0000

    deploy: f127f49562581b4842ef22e7a25e94c64804be3c

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://anacc22.github.io/another_graph_editor/",
-})
