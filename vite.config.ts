e26c93ca90e5239d490279e3c36f5a25faf32c5c
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-12 18:37:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://anacc22.github.io/another_graph_editor/",
+  base: "https://zjx-kimi.github.io/GraphEditorEN/",
 })
